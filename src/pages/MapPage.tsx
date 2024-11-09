f36741f36a64949ba3bd4a1488582142238f46a0
Mateo Piskac <mateopiskac2@gmail.com>
2024-11-09 14:13:01 +0100

    Add MapPage header

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from "../components/Header.tsx";
 
 const MapPage = () => {
   return (
-    <div>MapPage</div>
+      <><Header/>
+        <div>MapPage</div>
+      </>
   )
 }
 
