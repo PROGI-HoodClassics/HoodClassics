b4341e258d4ab5f3420e467a8b0b9aca81469aec
hrvojerados <131760001+hrvojerados@users.noreply.github.com>
2025-01-24 15:45:12 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.png'],
-  base: "http://localhost:8080/"
+  base: process.env.VITE_BASE || '/'
 })
