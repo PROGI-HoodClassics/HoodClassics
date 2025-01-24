00eb894b4f8f79603dce15d7e8523acd179443e2
Duje Ćubelić <duje.cubelic@fer.unizg.hr>
2025-01-24 16:03:24 +0100

    Fixed reports deleting

--- backend/src/main/java/hoodclassics/opp/dao/IsTaggedRepository.java
+++ backend/src/main/java/hoodclassics/opp/dao/IsTaggedRepository.java
@@ -2,11 +2,14 @@ package hoodclassics.opp.dao;
 
 import java.util.Optional;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hoodclassics.opp.domain.IsTagged;
 import hoodclassics.opp.domain.StoryIdTagIdKey;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IsTaggedRepository extends JpaRepository<IsTagged, StoryIdTagIdKey> {
-	
 }
--- backend/src/main/java/hoodclassics/opp/dao/ReportRepository.java
+++ backend/src/main/java/hoodclassics/opp/dao/ReportRepository.java
@@ -1,11 +1,18 @@
 package hoodclassics.opp.dao;
 
 import hoodclassics.opp.domain.Report;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
-
     List<Report> findAll();
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Report r WHERE r.storyId = :storyId")
+    void deleteByStoryId(@Param("storyId") Long storyId);
 }
